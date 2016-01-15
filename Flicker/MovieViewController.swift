3bbcb7b5f5c3cde929dcfba5b0e12093dd7e4ff2
kchow23 <kchow23@ufl.edu>
2016-01-14 19:24:15 -0500

    Initial

---
+++ Flicker/MovieViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MovieViewController.swift
+//  Flicker
+//
+//  Created by Kevin Chow on 1/14/16.
+//  Copyright © 2016 Kevin Chow. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+            cell.textLabel!.text = "row \(indexPath.row)"
+        print("row \(indexPath.row)")
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+target 'Flicker' do
+
+# This pulls in the latest version of AFNetworking >= 2.6 but less than 3.0.
+pod 'AFNetworking', '~> 2.6'
+
+end
